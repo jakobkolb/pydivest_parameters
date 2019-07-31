d79cf2ac437202239d39a009a5c102c520ee4e50
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2019-07-31 15:33:02 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -9,27 +9,28 @@ directory are modifications in the installed package.
 
 from setuptools import setup
 
-setup(name="pydivest_parameters",
-      version="0.0.1",
-      description="fitting parameters of the pydivest model to economic data",
-      url="to be added",
-      author="Jakob. J. Kolb",
-      author_email="kolb@pik-potsdam.de",
-      license="MIT",
-      packages=["pydivest_parameters"],
-      include_package_data=True,
-      install_requires=[
-            "numpy",
-            "scipy",
-            "pandas",
-            'sympy',
-      ],
-      package_data={
-          "pydivest_parameters.data.Energy_Consumption": ['*.xlsx'],
-          "pydivest_parameters.data.GDP": ['*.csv'],
-          "pydivest_parameters.data.Labor": ['*.csv'],
-          "pydivest_parameters.data.Oil_Price": ['*.xlsx'],
-      },
-      # see http://stackoverflow.com/questions/15869473/what-is-the-advantage-
-      # of-setting-zip-safe-to-true-when-packaging-a-python-projec
-      zip_safe=False)
+setup(
+    name="pydivestparameters",
+    version="0.0.1",
+    description="fitting parameters of the pydivest model to economic data",
+    url="to be added",
+    author="Jakob. J. Kolb",
+    author_email="kolb@pik-potsdam.de",
+    license="MIT",
+    packages=["pydivestparameters"],
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        'sympy',
+    ],
+    package_data={
+        "pydivest_parameters.data.Energy_Consumption": ['*.xlsx'],
+        "pydivest_parameters.data.GDP": ['*.csv'],
+        "pydivest_parameters.data.Labor": ['*.csv'],
+        "pydivest_parameters.data.Oil_Price": ['*.xlsx'],
+    },
+    # see http://stackoverflow.com/questions/15869473/what-is-the-advantage-
+    # of-setting-zip-safe-to-true-when-packaging-a-python-projec
+    zip_safe=False)
