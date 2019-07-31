31c6d0560a2403f2c873c031a84d834f15eaca8f
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2019-07-31 14:44:53 +0200

    initial commit for pydivest parameter fitting project

---
+++ pydivest_parameters/rs_models/econ_model_dirty_tech.py
@@ -0,0 +1,268 @@
+"""This is a simple two sector investment mode, that can be fitted to empirical
+data"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sympy as sp
+from tqdm import tqdm
+
+sp.init_printing()
+
+
+class econ_model():
+
+    # Define variables and parameters for the economic subsystem:
+
+    L, Lc, Ld = sp.symbols('L L_c L_d', positive=True, real=True)
+
+    K, Kc, Kd = sp.symbols('K K_c K_d', positive=True, real=True)
+    w, rc, rd = sp.symbols('w r_c r_d', positive=True, real=True)
+    R, G, Cc, Cd = sp.symbols('R, G, C_c C_d', positive=True, real=True)
+    eta, s, delta, chi, alpha, betac, betad, gammac, gammad = \
+        sp.symbols('eta s delta chi alpha beta_c beta_d gamma_c gamma_d',
+                   positive=True, rational=True, real=True)
+    mu = sp.symbols('mu', negative=True, rational=True, real=True)
+    bc, bd, bR, e, G0 = sp.symbols('b_c b_d b_R e G_0',
+                                   positive=True,
+                                   real=True)
+    Xc, Xd, XR = sp.symbols('X_c X_d X_R', positive=True, real=True)
+    Yc, Yd, cR = sp.symbols('Y_c Y_d c_R', positive=True, real=True)
+    dKc, dKd, dG, dC = sp.symbols(r'\dot{K}_c \dot{K}_d \dot{G} \dot{C}',
+                                  real=True)
+
+    # Solutions to the algebraic constraints from labor and capital markets
+
+    s1 = {
+        w: alpha * L**(alpha - 1) * (Xc + Xd * XR)**(1 - alpha),
+        rc: betac / Kc * Xc * L**alpha * (Xc + Xd * XR)**(-alpha),
+        rd: betad / Kd * Xd * XR * L**alpha * (Xc + Xd * XR)**(-alpha),
+        R: bd / e * Kd**betad * L**alpha * (Xd * XR / (Xc + Xd * XR))**alpha,
+        Lc: L * Xc / (Xc + Xd * XR),
+        Ld: L * Xd * XR / (Xc + Xd * XR),
+    }
+
+    s2 = {
+        Xc: (bc * Kc**betac * Cc**gammac)**(1. / (1 - alpha)),
+        Xd: (bd * Kd**betad * Cd**gammad)**(1. / (1 - alpha)),
+        XR: (1. - bR / e * (G / G0)**mu)**(1. / (1 - alpha))
+    }
+
+    outa_gas = bR / e * (G / G0)**mu
+
+    c_outputs = {
+        Yc: bc * Lc**alpha * Kc**betac * Cc**gammac,
+        Yd: bd * Ld**alpha * Kd**betad * Cd**gammad,
+        rc: rc,
+        rd: rd,
+        Cc: Cc,
+        G: G
+    }
+
+    c_kdots = {
+        dKc: s * eta * (w * L + rd * Kd + rc * Kc) - delta * Kc,
+        dKd: s * (1 - eta) * (w * L + rd * Kd + rc * Kc) - delta * Kd,
+        dG: -R,
+        dCc: bc * Lc**alpha * Kc**betac * Cc**gammac - chi * Cc
+        dCd: bd * Ld**alpha * Kd**betad * Cd**gammad - chi * Cd
+    }
+
+    c_expressions = [c_outputs, c_kdots]
+
+    for d in c_expressions:
+        for key, exp in d.items():
+            d[key] = exp.subs(s1).subs(s2)
+
+    def __init__(self,
+                 L: list,
+                 R: list,
+                 e: list,
+                 bc=1,
+                 bd=4,
+                 alpha=2 / 3,
+                 betac=1 / 3,
+                 betad=1 / 3,
+                 gammac=1 / 8,
+                 gammad=1 / 8,
+                 mu=-2,
+                 bR=1,
+                 G0=1000,
+                 s=0.25,
+                 delta=0.4,
+                 chi=0.2,
+                 eta=0.02):
+
+        self.parameter_substitutions = {
+            self.bc: bc,
+            self.bd: bd,
+            self.alpha: alpha,
+            self.betac: betac,
+            self.betad: betad,
+            self.gammac: gammac,
+            self.gammad: gammad,
+            self.mu: mu,
+            self.bR: bR,
+            self.G0: G0,
+            self.s: s,
+            self.delta: delta,
+            self.chi: chi,
+            self.eta: eta
+        }
+        self.kdots = {}
+        self.outputs = {}
+        self.expressions = [self.outputs, self.kdots]
+
+        for c_d, d in zip(self.c_expressions, self.expressions):
+            for key, exp in c_d.items():
+                d[key] = sp.lambdify(
+                    (self.L,
+                     self.Kc, self.Kd,
+                     self.Cc, self.Cd,
+                     self.R, self.G, self.e),
+                    exp.subs(self.parameter_substitutions), 'numpy')
+
+        # system time
+        self.t = 0
+
+        # energy intensity as a variable parameter
+        self.te = e
+
+        # trajectories from data
+        self.tL = L
+        self.tR = R
+
+        # trajectories from the model
+        self.tKc = []
+        self.tKd = []
+        self.tC = []
+        self.tG = []
+
+        self.t_outputs = []
+
+        # break condition
+        self.outa_gas = sp.lambdify(
+            (self.L, self.Kc, self.Kd, self.C, self.R, self.G, self.e),
+            self.outa_gas.subs(self.parameter_substitutions), 'numpy')
+
+    def initial(self, Kc0, Kd0, Cc0, Cd0, G0=None):
+        """initialize model with capital and knowledge stock.
+        resource stock optional.
+
+        Parameters
+        ----------
+        Kc0: float
+            initial capital stock in the clean sector
+        Kd0: float
+            initial capital stock in the dirty sector
+        Cc0: float
+            initial knowledge stock in the clean sector
+        Cd0: float
+            initial knowledge stock in the dirty sector
+        G0: float
+            initial fossil resource stock
+        """
+
+        self.tKc += [Kc0]
+        self.tKd += [Kd0]
+        self.tCc += [Cc0]
+        self.tCd += [Cd0]
+
+        if G0 is None:
+            self.tG += [self.G0]
+        else:
+            self.tG += [G0]
+
+    def _eval(self, L, Kc, Kd, Cc, Cd, R, G, e):
+        """evaluates model equations for given variable values
+        """
+
+        variables = (L, Kc, Kd, Cc, Cd, R, G, e)
+
+        if self.outa_gas(*variables) < 1:
+            dots = {key: exp(*variables) for key, exp in self.kdots.items()}
+            outputs = {
+                key: exp(*variables)
+
+                for key, exp in self.outputs.items()
+            }
+        else:
+            raise ValueError('outa gas')
+
+        return dots, outputs
+
+    def _step(self):
+        """integrates model one step forward in time
+        """
+
+        # set current value of energy intensity
+        e_t = self.te[self.t]
+
+        # set current value of variables
+        l_t = self.tL[self.t]
+        k_ct = self.tKc[self.t]
+        k_dt = self.tKd[self.t]
+        cc_t = self.tCc[self.t]
+        cd_t = self.tCd[self.t]
+        r_t = self.tR[self.t]
+        g_t = self.tG[self.t]
+
+        # get time derivatives of variables
+        dots, outputs = self._eval(L=l_t,
+                                   Kc=k_ct,
+                                   Kd=k_dt,
+                                   Cc=cc_t,
+                                   Cd=cd_t,
+                                   R=g_t,
+                                   G=g_t,
+                                   e=e_t)
+
+        # set next value of variables
+        k_ctp1 = k_ct + dots[self.dKc]
+        k_dtp1 = k_dt + dots[self.dKd]
+        cc_tp1 = cc_t + dots[self.dCc]
+        cd_tp1 = cd_t + dots[self.dCc]
+        g_tp1 = g_t - r_t
+
+        # append to trajectories
+        self.tKc += [k_ctp1]
+        self.tKd += [k_dtp1]
+        self.tCc += [cc_tp1]
+        self.tCd += [cd_tp1]
+        self.tG += [g_tp1]
+
+        self.t_outputs += [[val for val in outputs.values()]]
+
+        self.t += 1
+
+    def run(self, t_max: int):
+        """run model until t=t_max
+        """
+
+        for _ in range(t_max):
+            self._step()
+
+            if self.tG[-1] <= 0:
+                return
+
+    def get_trajectory(self):
+        """return model trajectory
+        """
+
+        return pd.DataFrame(data=self.t_outputs,
+                            columns=self.outputs.keys()).astype(float)
+
+
+if __name__ == '__main__':
+    model = econ_model(L=range(1, 101),
+                       R=np.ones(101),
+                       e=101 * [10],
+                       bd=2,
+                       eta=.1)
+    model.initial(Kc0=1,
+                  Kd0=100,
+                  C0=1,
+                  G0=model.parameter_substitutions[model.G0])
+    model.run(t_max=100)
+    trj = model.get_trajectory()
+    trj[[model.Yc, model.Yd]].astype(float).plot()
+    plt.show()
---
+++ pydivest_parameters/rs_models/parameter_fit.py
@@ -0,0 +1,303 @@
+"""module to crudely fit pydivest parameters to historical econic and resource
+use data."""
+
+import numpy as np
+import sympy as sp
+import pandas as pd
+import os
+import sys
+from scipy.interpolate import interp1d
+from scipy.optimize import least_squares
+
+module_path = os.path.abspath(os.path.join('..'))
+
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from rs_models.econ_model import econ_model as em
+DATA_PATH = '../data/'
+
+
+class ParameterFit:
+    """fit parameters to data and return them in dict"""
+
+    def __init__(self, alpha=2./3., gamma=1./8., chi=0.02, s=0.25):
+        """save parameter values and load data
+
+        Parameters
+        ----------
+        alpha: float
+            labor elasticity in production function. also defines capital
+            elasticity in production function via alpha + beta = 1
+        gamma: float
+            elasticity of knowledge in clean sector production function
+        chi: float
+            depreciation rate of knowledge stock
+        """
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.chi = chi
+        self.s = s
+
+        self.datasets = {}
+
+        self._load_labor_data()
+        self._load_gdp_data()
+        self._load_energy_data()
+        self._load_oil_price_data()
+
+        self.fitted_parameters = {}
+
+    def _load_labor_data(self):
+        """load labor data and add it to datasets"""
+
+        labor = pd.read_csv(DATA_PATH +
+                            'Labor/API_SL_TLF_TOTL_IN_DS2_en_csv_v2_59582.csv',
+                            header=2)
+        labor = labor.drop([
+            'Country Code', 'Indicator Name', 'Indicator Code', 'Unnamed: 63'
+        ],
+                           axis=1)
+        labor.set_index('Country Name', inplace=True)
+        labor = labor.transpose().astype(float)
+        labor.drop(list(labor.index.values[0:5]), inplace=True)
+        labor['Total'] = labor.sum(axis=1) / 10.
+        labor.index = labor.index.astype(int)
+
+        self.datasets['labor'] = labor
+
+    def _load_energy_data(self):
+        """load energy data and add it to datasets"""
+
+        energy = pd.read_excel(DATA_PATH + 'Energy_Consumption/' +
+                               'bp-stats-review-2019-all-data.xlsx',
+                               sheet_name=1)
+        energy.set_index('In Mtoe', inplace=True)
+        energy = energy.dropna().transpose()
+
+        self.datasets['energy'] = energy
+
+    def _load_gdp_data(self):
+        """load gdp data and add it to datasets"""
+
+        gdp = pd.read_csv(DATA_PATH +
+                          'GDP/API_NY_GDP_MKTP_CD_DS2_en_csv_v2_40924.csv',
+                          header=2)
+        gdp = gdp.drop([
+            'Country Code', 'Indicator Name', 'Indicator Code', 'Unnamed: 63'
+        ],
+                       axis=1)
+        gdp.set_index('Country Name', inplace=True)
+        gdp = gdp.transpose().astype(float)
+        gdp.drop(list(gdp.index.values[0:5]), inplace=True)
+        gdp['Total'] = gdp.sum(axis=1)
+        gdp.index = gdp.index.astype(int)
+
+        self.datasets['gdp'] = gdp
+
+    def _load_oil_price_data(self):
+        """load oil price data and add it to datasets"""
+
+        # load historical oil prices to approximate historical fossil resource
+        # cost.
+
+        # MWV. (July 25, 2019). Average annual OPEC crude oil price from 1960
+        # to 2019 (in U.S. dollars per barrel) [Chart].
+        # In Statista. Retrieved July 25, 2019,
+        # from https://www.statista.com/statistics/262858/change-in-opec-crude-oil-prices-since-1960/
+
+        oil_price = pd.read_excel(
+            DATA_PATH + 'Oil_Price/' +
+            'statistic_id262858_opec-oil-price-annually-1960-2019.xlsx',
+            sheet_name='Data',
+            header=4)
+        oil_price.index = oil_price.index.droplevel(0).astype(int)
+        oil_price.head()
+
+        # convert to price per ton
+        toe_to_barrel = 7.1428571428571
+        oil_price['Average price in U.S. dollars per ton'] = \
+            oil_price['Average price in U.S. dollars per barrel'] \
+            * toe_to_barrel
+
+        self.datasets['oil price'] = oil_price
+
+    def fit_params(self):
+        """fit parameters to data given the input parameters provided
+        """
+
+        energy = self.datasets['energy']
+        labor = self.datasets['labor']
+        gdp = self.datasets['gdp']
+        oil_price = self.datasets['oil price']
+        alpha = self.alpha
+        betac = 1-self.alpha
+        betad = betac
+        gamma = self.gamma
+        chi = self.chi
+        s = self.s
+
+        data = energy.loc[:,['Fraction Fossil', 'Fraction Renewable', 'Total Fossil', 'Total Renewable']]
+        data['labor'] = labor['Total']
+        data['production dirty'] = gdp['Total'] * energy['Fraction Fossil']
+        data['production clean'] = gdp['Total'] * energy['Fraction Renewable']
+
+        # interpolate labor data
+        dl = data.loc[data['labor']>0]
+        itp = interp1d(dl.index.values, dl['labor'].values, fill_value='extrapolate')
+        data['labor'] = [float(itp(x)) for x in data.index.values]
+
+        # calculate energy intensity
+        data['energy intensity'] = data['production dirty'] / data['Total Fossil']
+
+        # capital income ratio 2010
+        cir2010 = 4.4
+
+        # capital in each sector as production in each sector times capital income ratio.
+
+        clean_capital_2010 = cir2010 * data['production clean'].loc[2010]
+        dirty_capital_2010 = cir2010 * data['production dirty'].loc[2010]
+
+        # set capital depreciation such, that the capital stock is in equilibrium given the current income and savings rate.
+
+        # \dot{K} = s * income - delta * K == 0
+        # e.g. delta = s * income/K = s/capital income ratio
+
+        delta = s/cir2010
+
+        # calculate C according to 
+        # \dot{C} = Yc - chi * C, chi = 0.02
+
+        chi = self.chi
+        C = 0
+
+        for Yc in data.loc[range(1965,2011), 'production clean']:
+            C += Yc - chi * C
+
+        # Estimate current and initial resource stock from fossil resource usage.
+
+        fossil_data = data.loc[data['Total Fossil']>0, 'Total Fossil']
+
+        # cumulative historical usage
+        R_cum = 0
+        R_cum_t = []
+        for R in list(fossil_data.values):
+            R_cum += R
+            R_cum_t.append(R_cum)
+
+        # total stock estimated as cumulative historical usage plus another 100 years of current usage:
+        G1 = 100*fossil_data.values[-1]
+        G0 = R_cum + G1
+
+        # timeseries of fossil resource data as initial resource minus cumulative resource usage
+        data['Fossil Resource'] = (G0 - R_cum_t)
+
+        # so, this rough estimate says, that we have used about one third of the total available amount of fossil fuels. Fair enough.
+
+        # calculate approx total energy cost as price per ton * total fossil use per year in tons of oil equivalent
+        data['Fossil resource cost data'] = oil_price['Average price in U.S. dollars per ton'] * data['Total Fossil'] * 10e6
+        data['dirty production minus resource cost'] = data['production dirty'] - data['Fossil resource cost data']
+
+        from scipy.optimize import least_squares
+
+        def model(*args, **kwargs):
+
+            [bR, mu] = args[0]
+
+            cRm = [bR * r * (g / kwargs['G0'])**mu for r, g in zip(kwargs['R'], kwargs['G'])]
+
+            return [x1 - x2 for x1, x2 in zip(cRm, kwargs['cR'])]
+
+        x0 = (10e15, -2)
+        xlower = (0, -8)
+        xupper = (10e18, -2)
+
+        res = least_squares(model,
+                            x0,
+                            bounds=(xlower, xupper),
+                            kwargs={'G0': G0,
+                                    'G': list(data['Fossil Resource'].values),
+                                    'R': list(data['Total Fossil'].values),
+                                    'cR': list(data['Fossil resource cost data'])
+                                   })
+
+        data['Fossil resource cost fit'] = [res['x'][0] * r * (g / G0)**res['x'][1] for r, g in zip(list(data['Total Fossil'].values),
+                                                                                              list(data['Fossil Resource'].values))]
+        bR, mu = res['x']
+        data.head()
+
+        # implement production functions for Yc & Yd
+
+        Yc = em.c_expressions[0][em.Yc]
+        Yd = em.c_expressions[0][em.Yd]
+
+        parameter_substitutions = {em.C: C,
+                                   em.G0: G0,
+                                   em.G: G1,
+                                   em.bR: bR,
+                                   em.mu: mu,
+                                   em.Kc: clean_capital_2010,
+                                   em.Kd: dirty_capital_2010,
+                                   em.e: data.loc[2010, 'energy intensity'],
+                                   em.alpha: alpha,
+                                   em.betac: betac,
+                                   em.betad: betac,
+                                   em.gamma: gamma,
+                                   em.R: data.loc[2010,'Total Fossil'],
+                                   em.L: data.loc[2010, 'labor']}
+
+        fYc = Yc.subs(parameter_substitutions)
+        fYd = Yd.subs(parameter_substitutions)
+        [fYc, fYd]
+
+        from scipy.optimize import root
+        def rfoo(x, Ycd, Ydd):
+            psubs = {em.bc: x[0],
+                     em.bd: x[1]}
+            res = [sp.re(sp.Abs(fYc.subs(psubs) - Ycd).evalf()), sp.re(sp.Abs(fYd.subs(psubs) - Ydd).evalf())]
+            return res
+
+        x_start = (1, 1)
+
+        res = root(rfoo, x_start, args=(data.loc[2010, 'production clean'], data.loc[2010, 'production dirty']))
+        bc, bd = res['x']
+
+        # rescale bc and bd such that they are independend of the initial values of Kc, Kd, L and C and also independent of the input elasticities.
+
+        nbc = bc * data.loc[2010, 'labor']**alpha * clean_capital_2010**betac * C**gamma
+        nbd = bd * data.loc[2010, 'labor']**alpha * dirty_capital_2010**betad
+
+        fitted_parameters = {'Kc0': clean_capital_2010,
+                             'Kd0': dirty_capital_2010,
+                             'L': data.loc[2010, 'labor'],
+                             'C0': C,
+                             'G0': G0,
+                             'G1': G1,
+                             'bR': bR,
+                             'mu': mu,
+                             'bc': bc,
+                             'bd': bd,
+                             's': s,
+                             'delta': delta,
+                             'e': data.loc[2010, 'energy intensity'],
+                             'alpha': alpha,
+                             'betac': betac,
+                             'betad': betad,
+                             'gamma': gamma,
+                             'nbc': nbc,
+                             'nbd': nbd}
+
+        self.fitted_parameters = fitted_parameters
+
+    def get_params(self):
+        """return dictionary of fitted parameters and initial conditions
+        """
+        return self.fitted_parameters
+
+
+if __name__ == '__main__':
+
+    params = ParameterFit(chi=0.1, gamma=0.1)
+    params.fit_params()
+    print(params.get_params())
